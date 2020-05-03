4549fba6d876fb65372995c84f647dcc96c836ea
Ubuntu <ubuntu@ip-172-31-89-152.ec2.internal>
2020-05-03 09:47:10 +0000

    committing all files to github

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-    $("tr:odd").addClass("odd");
-    $("tr:even").addClass("even");
+
+    $("th").click(function() {
+        $("tr").children().removeClass("selection")
+        $(this).siblings().addClass("selection");
+
+    });
 });
